9761b433749314ef2bc7521e0a859c50a5e69695
Jordan BUSQUET <jordan.busquet@gmail.com>
2025-03-20 16:06:00 +0100

    version 1

---
+++ README.md
@@ -0,0 +1,26 @@
+# 🎮 Morpion en C
+
+Un petit Morpion réaliser en C qui est jouable en console.
+
+## 📌 Description
+
+Ce petit projet est l'implémentation du jeu du Morpion (Tic Tac Toe).
+Il consiste en un affrontement joueur contre joueur (jcj) dont l'objectif est d'aligner 3 signes identique dans une grille de 3 par 3.
+
+## 🚀 Fonctionnalités
+
+- Joueur VS Joueur (local)
+- Jeu en console
+- Vérification automatique de victoire
+
+## 🛠️ Technologies utilisées
+
+- **Language** : C
+- **Compilation** : GCC ou autre compilateur C compatible
+- **Exécution** : Terminal ou Console
+
+## 📥 Installation et Compilation
+
+```sh
+git clone https://github.com/Jojo12369/Morpion-C.git
+```
---
+++ morpion.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+char plateau(char cases[]) {
+    for (int i = 0; i < 3; i++) {
+        printf(" %c | %c | %c\n", cases[(2 - i) * 3], cases[(2 - i) * 3 + 1], cases[(2 - i) * 3 + 2]);
+        if (i < 2) {
+            printf("---+---+---\n");
+        }
+    }
+}
+
+void victoire(char cases[], int joueur) {
+    plateau(cases);
+    printf("joueur %d a gagner !", (joueur + 1));
+}
+
+void morpion() {
+    char symboles[2] = {'X', 'O'};
+    char cases[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    int tour = 0;
+    while (1) {
+        int joueur = tour % 2;
+        int choix;
+
+        plateau(cases);
+
+        printf("choisir une case joueur %d (1-9) : ", (joueur + 1));
+        scanf("%d", &choix);
+        while (cases[choix - 1] != ' ') {
+            printf("case invalide, choisir une case joueur %d (1-9) : ", (joueur + 1));
+            scanf("%d", &choix);
+        }
+        cases[choix - 1] = symboles[joueur];
+        tour++; 
+        for (int i = 0; i < 3; i++) {
+            if (cases[i * 3] == symboles[joueur] && cases[i * 3 + 1] == symboles[joueur] && cases[i * 3 + 2] == symboles[joueur]) {
+                victoire(cases, joueur);
+                return;
+            }
+            if (cases[i] == symboles[joueur] && cases[i + 3] == symboles[joueur] && cases[i + 6] == symboles[joueur]) {
+                victoire(cases, joueur);
+                return;
+            }
+        }
+        if (cases[0] == symboles[joueur] && cases[4] == symboles[joueur] && cases[8] == symboles[joueur]) {
+            victoire(cases, joueur);
+            return;
+        }
+        if (cases[6] == symboles[joueur] && cases[4] == symboles[joueur] && cases[2] == symboles[joueur]) {
+            victoire(cases, joueur);
+            return;
+        }
+        if (tour == 9) {
+            plateau(cases);
+            printf("Match nul !");
+            return;
+        }
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    morpion();
+
+    return 0;
+}
