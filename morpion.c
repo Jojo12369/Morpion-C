573e24d0e1f9f93d26d690fc89171538445be5b0
Jordan BUSQUET <jordan.busquet@gmail.com>
2025-03-20 16:58:48 +0100

    version 1.0.0

--- morpion.c
+++ morpion.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Fonction permetant d'afficher le plateau de jeu
 char plateau(char cases[]) {
     for (int i = 0; i < 3; i++) {
         printf(" %c | %c | %c\n", cases[(2 - i) * 3], cases[(2 - i) * 3 + 1], cases[(2 - i) * 3 + 2]);
@@ -9,11 +10,13 @@ char plateau(char cases[]) {
     }
 }
 
+// Fonction permetant d'afficher le message de victoire
 void victoire(char cases[], int joueur) {
     plateau(cases);
     printf("joueur %d a gagner !", (joueur + 1));
 }
 
+// Fonction permetant de gérer la partie
 void morpion() {
     char symboles[2] = {'X', 'O'};
     char cases[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
@@ -32,16 +35,24 @@ void morpion() {
         }
         cases[choix - 1] = symboles[joueur];
         tour++;
+
+        // Cette partie gère les victoires
         for (int i = 0; i < 3; i++) {
+
+            // Ici ce fait la gestion des victoires en lignes
             if (cases[i * 3] == symboles[joueur] && cases[i * 3 + 1] == symboles[joueur] && cases[i * 3 + 2] == symboles[joueur]) {
                 victoire(cases, joueur);
                 return;
             }
+
+            // Ici ce fait la gestion des victoires en colonnes
             if (cases[i] == symboles[joueur] && cases[i + 3] == symboles[joueur] && cases[i + 6] == symboles[joueur]) {
                 victoire(cases, joueur);
                 return;
             }
         }
+
+        // Ici ce fait la gestion des victoires en diagonales
         if (cases[0] == symboles[joueur] && cases[4] == symboles[joueur] && cases[8] == symboles[joueur]) {
             victoire(cases, joueur);
             return;
@@ -50,6 +61,8 @@ void morpion() {
             victoire(cases, joueur);
             return;
         }
+
+        // Cette partie gère les matchs nuls
         if (tour == 9) {
             plateau(cases);
             printf("Match nul !");
